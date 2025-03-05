9602d14e2661ca85612f94b1e6a30ccb876b0472
Maks-Ytka <maksym.kozlovskiy@gmail.com>
2025-03-05 17:10:45 +0200

    Added HTTP server with route /time (rebased)

--- main.go
+++ main.go
@@ -10,6 +10,11 @@ type TimeResponse struct {
 	Time string `json:"time"`
 }
 
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(TimeResponse{Time: time.Now().Format(time.RFC3339)})
+}
+
 func main() {
 	println("Hello world!")
 }
