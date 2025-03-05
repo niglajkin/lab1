d1741ed95408e51b6f6765eff98b5c888d609c8a
Ihlaikin Mykola <ggg501434@gmail.com>
2025-03-05 16:51:47 +0200

    Added TimeResponse struct (fast-forward)

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	"encoding/json"
-	"net/http"
-	"time"
+  	"encoding/json"
+  	"net/http"
+  	"time"
 )
+
+type TimeResponse struct {
+	Time string `json:"time"`
+}
 
 func main() {
 	println("Hello world!")
